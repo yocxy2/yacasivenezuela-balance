11bab5039740ff7ceb1e9fc71ec8522ed979052b
Keryc Diaz <kerycdiaz@gmail.com>
2024-09-16 18:56:28 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Ya Casi Venezuela (Balance)`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
