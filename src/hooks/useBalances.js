194ef148b8dc0817193b011402b9da2f1a14b0c5
Keryc Diaz <kerycdiaz@gmail.com>
2024-09-16 21:15:21 -0600

    update in vivo

--- src/hooks/useBalances.js
+++ src/hooks/useBalances.js
@@ -1,3 +1,5 @@
+// src/hooks/useBalances.js
+
 import { useEffect, useState } from 'react';
 import {
   getBitcoinBalance,
@@ -18,6 +20,8 @@ const useBalances = () => {
   });
 
   useEffect(() => {
+    let intervalId;
+
     const fetchBalances = async () => {
       const btcAddress = '1P5jycZKohMwvHZB8JYuLLMMBy73DoUeDx';
       const ethAddress = '0xc2c48ae6d42f17823c6263a57c5630821f88b912';
@@ -37,11 +41,13 @@ const useBalances = () => {
           getCryptoPrices(),
         ]);
 
+        // Calcular el valor en USD de cada criptomoneda
         const btcUSD = btcBalance * prices.bitcoin.usd;
         const ethUSD = ethBalance * prices.ethereum.usd;
         const usdcUSD = usdcBalance * prices['usd-coin'].usd;
         const usdtUSD = usdtBalance * prices.tether.usd;
 
+        // Calcular el saldo total en USD
         const totalUSD = btcUSD + ethUSD + usdcUSD + usdtUSD;
 
         setBalances({
@@ -57,10 +63,18 @@ const useBalances = () => {
       }
     };
 
+    // Llamada inicial
     fetchBalances();
+
+    // Establecer intervalo para actualizar los saldos
+    intervalId = setInterval(fetchBalances, 60000); // Actualiza cada 60 segundos
+
+    // Limpiar el intervalo al desmontar
+    return () => clearInterval(intervalId);
   }, []);
 
   return balances;
 };
 
 export default useBalances;
+
